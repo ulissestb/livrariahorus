e71b18c11dce52597c95c6d11ac18021df233b61
nilosegio <nilosergiojr637@gmail.com>
2018-08-15 21:31:43 -0300

    Criando a pagina de login by Nilo

---
+++ horus/login.php
@@ -0,0 +1,15 @@
+<?php
+require('index.php');
+?>
+
+<div style="margin: 100px; margin-top: 0px;">
+    <h3>Página de Login</h3>
+    <form clas="form-group" method="post" action="autenticacao.php">
+        <label>Login:</label><br>
+        <input type="text" placeholder="Ex. Nilo" class="form-control">
+        <label>Senha:</label>
+        <input type="password" class="form-control"><br><br>
+        <input type="submit" class="btn btn-success" value="Entrar">
+    </form>
+
+</div>
