9dec73bdbed99c0d54ea0dcd1903e444b40e50ab
darkflamesheep <darkflamesheep@gmail.com>
2018-08-15 21:08:43 -0300

    adicionando a pagina de contato by pedro

---
+++ horus/contato.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" lang="pt-br">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head> 
+
+<body>
+<div id="fundo"> </div>
+
+
+<div id="total">
+    <header id="logcad">
+        <div id="cabecalho">
+            <div id="login">
+                <a href="login.php">Entrar  |</a>
+                <a href="cadastro.php">Cadastrar</a>
+            </div>
+
+        <div id="logo"><img src="css/img/LogoLivrariaHorus.png"></div>
+        </div>
+    </header>
+    
+    
+    <!--BARRA DE NAVEGAÇÃO -->
+    <nav id="navbar" class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">Horus</a>
+    <a class="navbar-brand" href="index.php">Home</a>
+    <a class="navbar-brand" href="#">Livros</a>
+    <a class="navbar-brand" href="contato.php">Contatos</a>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-warning my-2 my-sm-0" type="submit">Procurar</button>
+    </form>
+    </nav>
+    <!--FIM DA BARRA DE NAVEGAÇÃO-->
+    
+   <!-- inicio contigo contato by: Pedrin -->
+
+    <div id="contat" class="container">
+        <div id="dadoslivraria"> 
+           <h3>Quem Somos</h3>
+            <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, necessitatibus, est? Quaerat explicabo laudantium voluptatem voluptates nemo fugit aspernatur consequuntur esse voluptas quibusdam maxime aliquid ipsum eum vitae assumenda, quod voluptate accusantium molestias consequatur voluptatum cumque quia quae cum! Voluptate laboriosam laudantium atque, molestias dicta adipisci tempore quas. Facere repellat sit dicta minus, ullam voluptatum quasi ipsum explicabo ratione sunt impedit saepe. Fugiat alias nemo iusto saepe minus quisquam architecto mollitia illo fugit dignissimos quae hic sequi qui porro debitis, nobis totam! Sint explicabo perspiciatis magni odio hic, veniam dignissimos iste illo, dolorem sit esse, itaque obcaecati voluptate. Laboriosam deserunt voluptatibus dolores quo non id assumenda harum in atque fugit provident nisi sequi voluptas nobis ratione magnam, sunt cum iusto. Non perferendis, repellendus necessitatibus, aliquam cupiditate voluptatum ab odit accusantium quasi sunt vitae ducimus quibusdam velit quidem, sequi est eum iste! Rem molestias fugit pariatur eaque quod natus maiores accusantium.</span></p>
+
+            
+        </div>
+          <div class="form-group" id="contatenos">
+          
+          <h3>Entre contato com a gente!!!</h3>
+             Rua Senador Dantas 75 sala 303
+             Centro 20031-914 
+             (21) 2533-3638<br>
+            <b>livhorus@gmail.com</b> <br>
+           Nome: <input type="text" name="nome" class="form-control"> <br>
+        
+            Email: <input type="email" name="email" class="form-control"><br>
+        
+            Descrição: <textarea class="form-control"></textarea><br>
+        
+               <input type="submit" class="btn btn-primary" value="Enviar">
+            </div>
+        
+    </div>
+    
+   
+<!-- FIM CÓDIGO CONTATO PEDRIN -->
+   
+<!--Código do Rodapé -->
+
+</div>
+    <script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+    
+</body>
+
+</html>
+
+
+
