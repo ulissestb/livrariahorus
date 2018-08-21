626011fd01e0b66bdbccb6a4a4e1d8f88a052938
alexiapudim <alexia.ferreirac@gmail.com>
2018-08-21 20:58:04 -0300

    adicionado página de cadastro e cadastro ao banco  by: Lexie

---
+++ horus/config/post.php
@@ -0,0 +1,20 @@
+<?php
+$nome           = $_POST['nome'];
+$sobrenome      = $_POST['sobrenome'];
+$datanascimento = $_POST['datanascimento'];
+$celular        = $_POST['celular'];
+$email          = $_POST['email'];
+
+$nomeTodo = $nome ." ". $sobrenome;
+
+include('Sql.php');
+
+$dados = "INSERT INTO cliente(telefone, nome ,data, email) VALUES ('{$celular}','{$nomeTodo}','{$datanascimento}','{$email}')";
+    
+mysqli_query($conn,$dados); 
+
+mysqli_close($conn);
+        
+?>
+
+<a href="../cadastrar.php">Voltar</a>
