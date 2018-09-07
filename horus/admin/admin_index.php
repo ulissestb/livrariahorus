ff4621420042ac216652f4d7d6e6a4b3ffde3298
ulissestb <ulisses.melo.nascimento@gmail.com>
2018-09-07 17:54:04 -0300

    Terminando parte visual do projeto

--- horus/admin/admin_index.php
+++ horus/admin/admin_index.php
@@ -1,57 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <script src="../js/jquery-3.3.1.min.js"></script>
-    <script src="../js/bootstrap.js"></script>
-    <title>Livraria Horus</title>
-</head>
-<body>
-<div id="total">
-    <header id="logcad">
-        <div id="cabecalho">
-            <div id="login">
-                <a href="login.php">Entrar  |</a>
-                <a href="cadastro.php">Cadastrar</a>
-            </div>
-
-            <h1>Livraria Horus</h1>
-        </div>
-    </header>
+<?php include('header-admin.html') ?>
+    <div class="container">
+       
     
 
-    <!--BARRA DE NAVEGAÇÃO -->
-    <nav id="navbar" class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Horus</a>
-    <div class="dropdown show">
-  <a class="navbar-brand" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Livros
-  </a>
-
-  <div class="dropdown-menu"  id="dropdown" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" id="dropdown" href="admin_adicionarlivros.php">Adicionar</a>
-    <a class="dropdown-item" id="dropdown" href="admin_alterarlivros.php">Alterar</a>
-    <a class="dropdown-item" id="dropdown" href="#">Remover</a>
-  </div>
-</div>
-  
-    <a class="navbar-brand" href="#">Clientes</a>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-warning my-2 my-sm-0" type="submit">Procurar</button>
-    </form>
-    </nav>
-    <!--FIM DA BARRA DE NAVEGAÇÃO-->
-
-
-    <div class="container">
-        
+    
     </div>
-
-     </div>
-</body>
-</html>
+<?php include('footer-admin.html')?>
--- horus/index.php
+++ horus/index.php
@@ -2,10 +2,64 @@
 
     <div class="container">
     
-       
+        <h3>Bem-vindo a Livraria Horus!</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus vitae voluptatibus, quaerat, inventore fugit at iure reiciendis rerum quas sequi ratione quis esse officia, sed vero fugiat. Facere, molestiae mollitia.
+            Corporis, dolorum nisi voluptates error molestiae velit libero provident non facere voluptatem optio quia culpa tempore tempora corrupti ratione nobis rerum amet, repudiandae unde nemo suscipit quidem. Ut, inventore quo!
+            Quisquam dolores, dolor numquam eius necessitatibus, fuga, facilis odit non nobis sequi eligendi atque nulla. Enim architecto dignissimos qui animi tenetur iure praesentium quasi voluptatum ipsam, fugit maiores nobis omnis.
+            Officiis autem doloribus soluta deleniti natus nihil exercitationem dolorem optio! Suscipit et hic repellendus neque doloremque delectus fugit quibusdam. Perspiciatis eligendi suscipit laboriosam asperiores unde! A deserunt natus cupiditate adipisci?
+            Debitis, blanditiis sapiente asperiores maiores incidunt voluptates corrupti cupiditate ad sint eaque. Tempore sequi hic maiores voluptate ipsum molestiae harum distinctio, dolor ullam delectus perferendis aspernatur ratione repellendus nihil tempora?
+            Doloribus quaerat quae ab in molestiae nemo dolorum itaque repudiandae? Quae, repudiandae commodi! Amet nesciunt molestias sed architecto rerum esse minus voluptas aliquid dolorem iste reiciendis, accusantium minima nihil reprehenderit.</p>       
+
+    <br>
+    <div id="carousel-slide" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-slide" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-slide" data-slide-to="1"></li>
+            <li data-target="#carousel-slide" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img id="img-slide" class="d-block w-100" src="img/imagem6.png" alt="traga seus livros">
+            </div>
+            <div class="carousel-item">
+                <img  id="img-slide" class="d-block w-100" src="img/traga-seus-livros.jpg" alt="conheça nossos serviços">
+            </div>
+            <div class="carousel-item">
+                <img id="img-slide" class="d-block w-100" src="img/banner.png" alt="conheça nossos serviços">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carousel-slide" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel-slide" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Proximo</span>
+        </a>
+    </div>
+
     
+        <br><br> <hr> <br><br>
+    <div id="conheca-horus">
+        <h3>Conheça a Livraria Horus</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum possimus, eaque dolorum numquam officia accusantium exercitationem iste perspiciatis repellat harum tenetur temporibus, ipsam maxime rerum incidunt natus. Impedit, debitis deserunt!</p>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="img/livraria1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/livraria2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/livraria3.jpg" alt="Third slide">
+            </div>
+        </div>
+    </div>
 
+    </div>
 
+    <br><hr>    <br>
     <div id="dadoslivraria"> 
            <h3>Quem Somos</h3>
             <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, necessitatibus, est? Quaerat explicabo laudantium voluptatem voluptates nemo fugit aspernatur consequuntur esse voluptas quibusdam maxime aliquid ipsum eum vitae assumenda, quod voluptate accusantium molestias consequatur voluptatum cumque quia quae cum! Voluptate laboriosam laudantium atque, molestias dicta adipisci tempore quas. Facere repellat sit dicta minus, ullam voluptatum quasi ipsum explicabo ratione sunt impedit saepe. Fugiat alias nemo iusto saepe minus quisquam architecto mollitia illo fugit dignissimos quae hic sequi qui porro debitis, nobis totam! Sint explicabo perspiciatis magni odio hic, veniam dignissimos iste illo, dolorem sit esse, itaque obcaecati voluptate. Laboriosam deserunt voluptatibus dolores quo non id assumenda harum in atque fugit provident nisi sequi voluptas nobis ratione magnam, sunt cum iusto. Non perferendis, repellendus necessitatibus, aliquam cupiditate voluptatum ab odit accusantium quasi sunt vitae ducimus quibusdam velit quidem, sequi est eum iste! Rem molestias fugit pariatur eaque quod natus maiores accusantium.</span></p>
@@ -20,3 +74,4 @@
         
     </div>
             <?php include('footer.html') ?>
+
