ddfb97d0a2f0d38083ff764e1f97d0cb9ab26701
ulissestb <ulisses.melo.nascimento@gmail.com>
2018-09-07 14:51:02 -0300

    Imagens dos livros

--- horus/index.php
+++ horus/index.php
@@ -5,7 +5,7 @@
        
     
 
-    
+
     <div id="dadoslivraria"> 
            <h3>Quem Somos</h3>
             <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, necessitatibus, est? Quaerat explicabo laudantium voluptatem voluptates nemo fugit aspernatur consequuntur esse voluptas quibusdam maxime aliquid ipsum eum vitae assumenda, quod voluptate accusantium molestias consequatur voluptatum cumque quia quae cum! Voluptate laboriosam laudantium atque, molestias dicta adipisci tempore quas. Facere repellat sit dicta minus, ullam voluptatum quasi ipsum explicabo ratione sunt impedit saepe. Fugiat alias nemo iusto saepe minus quisquam architecto mollitia illo fugit dignissimos quae hic sequi qui porro debitis, nobis totam! Sint explicabo perspiciatis magni odio hic, veniam dignissimos iste illo, dolorem sit esse, itaque obcaecati voluptate. Laboriosam deserunt voluptatibus dolores quo non id assumenda harum in atque fugit provident nisi sequi voluptas nobis ratione magnam, sunt cum iusto. Non perferendis, repellendus necessitatibus, aliquam cupiditate voluptatum ab odit accusantium quasi sunt vitae ducimus quibusdam velit quidem, sequi est eum iste! Rem molestias fugit pariatur eaque quod natus maiores accusantium.</span></p>
@@ -16,23 +16,7 @@
             <img src="img/banner.png" alt="">
         </div>
         
-        <div id="quadro1">
-            <img src="img/primeiro1.jpg" alt="">
-            
-        </div>
-        
-        <div id="quadro2">
-          <img src="img/segundo1.jpg" alt="">
-            
-        </div>
-        
-        <div id="video">
-            <h1>aqui video</h1>
-        </div>
-        <div id="livro">
-            <h1>aqui vai o livro</h1>
-        </div>
-        
+
         
     </div>
             <?php include('footer.html') ?>
